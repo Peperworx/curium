fcd018f9e00e8b93a11da12b42e042ba3e90f0dc
Riley Wilton <meetingprince34@outlook.com>
2020-12-23 23:47:42 -0500

    Changed vuepress theme

--- docs/src/.vuepress/config.js
+++ docs/src/.vuepress/config.js
@@ -66,5 +66,10 @@ module.exports = {
   plugins: [
     '@vuepress/plugin-back-to-top',
     '@vuepress/plugin-medium-zoom',
-  ]
+  ],
+
+  theme: 'vuepress-theme-succinct',
+  globalUIComponents: [
+    'ThemeManager'
+  ],
 }
