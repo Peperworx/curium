366a8cc454d2f75d09a4f9ce663ecacf9bbc78e2
Riley Wilton <meetingprince34@outlook.com>
2021-01-01 12:31:44 -0500

    Working on new parser system

--- curium/parser/tree.py
+++ curium/parser/tree.py
@@ -1,72 +1,129 @@
-class Expr:
+class expr:
     pass
 
-class BinOp(Expr):
-    def __init__(self, op, left, right, lineno, index):
+
+class statement:
+    pass
+
+class namespace(expr):
+    def __init__(self,
+            statements: list[statement]):
+        # The actual list of statements
+        self.statements = statements
+
+
+
+class binary_op(expr):
+    def __init__(self, 
+            op: str, 
+            left: expr, 
+            right: expr, 
+            lineno: int,
+            index: int):
+        # The operator
         self.op = op
+
+        # The left operand
         self.left = left
+
+        # The right operand
         self.right = right
-        self.lineno = lineno
-        self.index = index
-class Initialize(Expr):
-    def __init__(self, new, type, value, lineno, index):
-        self.new = new
-        self.type = type
-        self.value = value
+
+        # The linenumber and index
         self.lineno = lineno
         self.index = index
 
-
-
-class UnOp(Expr):
-    def __init__(self, op, right, lineno, index):
+class unary_op(expr):
+    def __init__(self, 
+            op: str, 
+            operand: expr, 
+            lineno: int, 
+            index: int):
+        # The operator
         self.op = op
-        self.right = right
+
+        # The operand
+        # This does not have a side because there are both
+        # Prefix and postfix unary operators
+        self.operand = operand
+
+        # The linenumber and index
         self.lineno = lineno
         self.index = index
 
-class Group(Expr):
-    def __init__(self, id, lineno, index, *args):
-        self.all = args
+class function_call(expr):
+    def __init__(self, 
+            name: str, 
+            arguments: expr, 
+            lineno: int, 
+            index: int ):
+        # The name of the function
+        self.name = name
+
+        # The arguments of the function.
+        # This is an expression
+        self.arguments = arguments
+
+        # The linenumber and index
         self.lineno = lineno
         self.index = index
 
+class function_literal(expr):
+    def __init__(self,
+            arguments: expr,
+            contents: namespace,
+            lineno: int,
+            index: int):
+        
+        # List of arguments taken
+        self.arguments = arguments
 
-class If(Expr):
-    def __init__(self,op,expr,conts,nextSet = None):
-        self.op = op
-        self.expr = expr
-        self.conts = conts
-        self.nextSet = nextSet
-
-class Elif(If):
-    pass
-
-class Else(Expr):
-    def __init__(self,op,conts):
-        self.op = op
-        self.conts = conts
-
-class EndNode(Expr):
-    def __init__(self, value, lineno, index):
-        self.value = value
-        self.lineno = lineno
-        self.index = index
-
-class Number(EndNode):
-    pass
-
-class String(EndNode):
-    pass
-
-class Name:
-    def __init__(self,value,template,lineno,index):
-        self.value = value
-        self.template = template
-        self.lineno = lineno
-        self.index = index
-
-class Function:
-    def __init__(self,contents,args):
+        # Contents of the function
         self.contents = contents
-        self.args = args
+
+        # The linenumber and index
+        self.lineno = lineno
+        self.index = index
+
+
+
+
+
+class var_initialize(statement):
+    def __init__(self, 
+            var_type: expr,
+            name: str):
+
+        # The type of the variable
+        self.var_type = var_type
+
+        # The name of the variable
+        self.name = name
+
+class var_assign(statement):
+    def __init__(self,
+            name: str,
+            value: expr):
+        
+        # The variable name
+        self.name = name
+
+        # The new value
+        self.value = value
+
+class var_init_assg(statement):
+    def __init__(self,
+            var_type: expr,
+            name: str,
+            value: expr):
+        
+        # The variable type
+        self.var_type = var_type
+
+        # The variable name
+        self.name = name
+
+        # The variable value
+        self.value = value
+
+
