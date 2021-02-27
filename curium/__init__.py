30f17c95482c556899303864e68267b7155d61ec
Riley Wilton <meetingprince34@outlook.com>
2021-02-26 20:11:33 -0500

    Switching to peg

--- curium/__init__.py
+++ curium/__init__.py
@@ -1,1 +1,9 @@
 ## Curium library entry
+from parsimonious.grammar import Grammar
+
+
+
+def parse(input):
+    with open("parse.lex") as f:
+        grammar = Grammar(f.read())
+    
--- test.py
+++ test.py
@@ -1,9 +1,8 @@
 
-from curium import parser
-from curium import lexer
 from curium import preprocessor
 from rich.console import Console
 from rich import print
+import curium
 import json
 
 
@@ -16,41 +15,8 @@ def test_file(filename: str,result: str):
         # We strip whitespace for consistancy.
         assert "".join(f.read().split()) == "".join(out.split())
     
-    # Now lex it
-    lex = lexer.Lex()
-    
-    # If there are no errors, lexing succeded
-    # Lets dump these to some lexdump files
-    lexdump = []
-    
-    for l in lex.tokenize(out):
-        lexdump.append(
-            {
-                "type":l.type,
-                "value":l.value,
-                "lineno":l.lineno,
-                "index":l.index,
-                "column":lexer.find_column(out,l)-1 # Remove one so that it starts at 1
-            }
-        )
-    
-    # Save the lexdump
-    with open(filename+".lexdump","w+") as f:
-        f.write(json.dumps(lexdump,indent=4))
 
-    # Now try to parse it
-    lexed = lex.tokenize(out)
-    parse = parser.Parse()
-    parse.text = out
-    parsed = parse.parse(lexed)
-    
-    # Now save the parsedump
-    parsedump = json.dumps(parsed.resolve(), indent=4)
-
-    with open(filename+".parsedump","w+") as f:
-        f.write(parsedump)
-
-
+    parsed = curium.parse(out)
 
 
 if __name__ == "__main__":
